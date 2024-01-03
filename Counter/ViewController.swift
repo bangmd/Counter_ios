175d4db9ea47e065892beb4eb618e1f5dbbc5fb4
Soslan <soslan74d@gmail.com>
2024-01-03 16:42:33 +0300

    Решение базовой задачи: - Добавлен  UILabel и UIButton

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var meanLabel: UILabel!
+    
+    private var counter: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        meanLabel.text = "Значение счётчика: \(counter)"
+        changeButton.setTitle("Увеличить на 1", for: .normal)
     }
-
-
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1
+        viewDidLoad()
+    }
+    
+    
+    
 }
-
